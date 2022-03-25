8ec8b45058af7fe6a1bfe2349886732f0651de22
dlaramie18 <dlaramie18@gmail.com>
2022-03-24 18:48:36 -0700

    Added classes for main window, data access, and the database

---
+++ CS3280-group-assignment/clsHandleError.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_group_assignment
+{
+    static class clsHandleError
+    {
+        /// <summary>
+        /// Static method to handle any errors in the program
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        public static void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                             "HandleError Exception: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ CS3280-group-assignment/clsMainLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_group_assignment
+{
+    class clsMainLogic
+    {
+    }
+}
