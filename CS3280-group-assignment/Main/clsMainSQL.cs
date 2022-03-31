e456ad70f0486272d819fee09009e3590115e6a6
dlaramie18 <dlaramie18@gmail.com>
2022-03-30 19:38:05 -0700

    Main window SQL

--- CS3280-group-assignment/Main/clsMainSQL.cs
+++ CS3280-group-assignment/Main/clsMainSQL.cs
@@ -9,18 +9,96 @@ namespace CS3280_group_assignment.Main
     class clsMainSQL
     {
         /// <summary>
-        /// This SQL gets all data on an invoice for given invoiceID
+        /// Update total cost of given invoice
         /// </summary>
-        /// <param name="sInvoiceID"></param>
+        /// <param name="TotalCost"></param>
+        /// <param name="InvoiceNum"></param>
         /// <returns></returns>
-        public string SelectInvoiceData(string sInvoiceID)
-
+        public string UpdateTotalCost(string TotalCost, string InvoiceNum)
         {
-
-            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID;
-
+            string sSQL = "UPDATE Invoices SET TotalCost = " + TotalCost + " WHERE InvoiceNum = " + InvoiceNum;
             return sSQL;
+        }
 
+        /// <summary>
+        /// Delete a line item for given invoice number and item code
+        /// </summary>
+        /// <param name="InvoiceNum"></param>
+        /// <returns></returns>
+        public string DeleteLineItem(string InvoiceNum, string ItemCode)
+        {
+            string sSQL = "DELETE From LineItems WHERE InvoiceNum = " + InvoiceNum + " AND ItemCode = " + ItemCode;
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Delete an invoice for given invoice number
+        /// </summary>
+        /// <param name="InvoiceNum"></param>
+        /// <returns></returns>
+        public string DeleteInvoice(string InvoiceNum)
+        {
+            string sSQL = "DELETE From Invoices WHERE InvoiceNum = " + InvoiceNum;
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Insert a row into LineItem
+        /// </summary>
+        /// <param name="InvoiceNum"></param>
+        /// <param name="LineItemNum"></param>
+        /// <param name="ItemCode"></param>
+        /// <returns></returns>
+        public string InsertLineItem(string InvoiceNum, string LineItemNum, string ItemCode)
+        {
+            string sSQL = "INSERT INTO LineItems(InvoiceNum, LineItemNum, ItemCode) Values(" + InvoiceNum + ", " +
+                            LineItemNum + ", '" + ItemCode + "')";
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Insert an invoice, autogenerate the invoice number
+        /// </summary>
+        /// <param name="InvoiceDate"></param>
+        /// <param name="TotalCost"></param>
+        /// <returns></returns>
+        public string InsertInvoice(string InvoiceDate, string TotalCost)
+        {
+            string sSQL = "INSERT INTO Invoices(InvoiceDate, TotalCost) Values('#" + InvoiceDate + "#', " +
+                            TotalCost + ")";
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Retrieves all data for a given invoice number
+        /// </summary>
+        /// <param name="InvoiceNum"></param>
+        /// <returns></returns>
+        public string SelectInvoiceData(string InvoiceNum)
+        {
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + InvoiceNum;
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Selects all items from ItemDesc
+        /// </summary>
+        /// <returns></returns>
+        public string SelectAllItems()
+        {
+            string sSQL = "SELECT * FROM ItemDesc";
+            return sSQL;
+        }
+
+        /// <summary>
+        /// Select all items under a given invoice
+        /// </summary>
+        /// <param name="InvoiceNum"></param>
+        /// <returns></returns>
+        public string SelectInvoiceItems(string InvoiceNum)
+        {
+            string sSQL = "SELECT LineItems.ItemCode, ItemDesc.ItemDesc, ItemDesc.Cost FROM LineItems, ItemDesc Where LineItems.ItemCode = ItemDesc.ItemCode And LineItems.InvoiceNum = " + InvoiceNum;
+            return sSQL;
         }
     }
 }
