d0046aa6bcf377d6cd04f23e3127a8fc2bc124d2
JazLava <jazminelava95@outlook.com>
2022-03-31 19:00:15 -0600

    Here we go!

--- CS3280-group-assignment/Search/clsSearchSQL.cs
+++ CS3280-group-assignment/Search/clsSearchSQL.cs
@@ -8,5 +8,80 @@ namespace CS3280_group_assignment.Search
 {
     class clsSearchSQL
     {
+        /// <summary>
+        /// "when the invoice is selected, the invoice ID is saved in a local variable that the main window can access"
+        /// </summary>
+     
+
+        public string SelectAllFromInvoice()
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices";
+
+            return sSQL;
+
+        }
+
+        public string SelectInvoiceData(string sInvoiceID)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID;
+
+            return sSQL;
+
+        }
+
+        public string SelectInvoiceDatanDate(string sInvoiceID, string InvoiceDate)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID + " AND InvoiceDate = " InvoiceDate;
+
+            return sSQL;
+
+        }
+
+        public string SelectInvoiceDatanDatenCost(string sInvoiceID, string InvoiceDate, string TotalCost)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID + " AND InvoiceDate = " InvoiceDate + " AND TotalCost = " TotalCost;
+
+            return sSQL;
+
+        }
+
+        public string SelectInvoiceCost(string TotalCost)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE TotalCost = " + TotalCost;
+
+            return sSQL;
+
+        }
+
+
+        public string SelectInvoiceCostnDate(string TotalCost, string InvoiceDate)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE TotalCost = " + TotalCost + " AND InvoiceDate = " InvoiceDate;
+
+            return sSQL;
+
+        }
+        
+        public string SelectInvoiceDate(string InvoiceDate)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceDate = " + InvoiceDate;
+
+            return sSQL;
+
+        }
     }
 }
